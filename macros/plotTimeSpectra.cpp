a1c83d149bdd9da05ab3b52450337618c113e7bb
TamaraJBabij <tamarababij@gmail.com>
2016-02-03 10:19:08 +1100

    Imported github PosRemi library to floobits and started timespectra histogram

---
+++ macros/plotTimeSpectra.cpp
@@ -0,0 +1,15 @@
+// creats histogram of time of flioghts (given by MCP signal) of the particles. 
+#include <iostream>
+#include "DataSet.h"
+#include "Group.h"
+#include "Hit.h"
+
+// returns histogram
+// return type, function name(argument)
+// where DataSet is returned from the tree reader
+
+
+
+TH1D plotTimeSpectra(DataSet dataset){
+
+}
