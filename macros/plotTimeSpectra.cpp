cabac81a9106f14b927a5839d62fcb809cf8bb6e
TamaraJBabij <tamarababij@gmail.com>
2016-03-10 16:56:44 +1100

    tried to get histogram to work

--- macros/plotTimeSpectra.cpp
+++ macros/plotTimeSpectra.cpp
@@ -11,26 +11,26 @@
 
 
 
-HistogramPair plotTimeSpectra(DataSet* dataset){
+HistogramPair plotTimeSpectra(DataSet* dataset) {
 	HistogramPair hist;
-	hist.positive = new TH1D("hpos","TimeSpectra positive",200,-100,32000);
-	hist.negative = new TH1D("hneg","TimeSpectra negative",200,-100,32000);
-	for(Group* group : dataset){
-		for(Hit& h : group){
-			if(h.channel == ChannelID::mcp){
-				if(h.detector == DetectorID::pos){
-					hist.positive->Fill(hit.time);
+	TH1D* histpositive = new TH1D("hpos", "TimeSpectra positive", 200, -100, 32000);
+	//histnegative = new TH1D ("hneg","TimeSpectra negative",200,-100,32000);
+	for (Group* group : dataset) {
+		for (Hit h : group) {
+			if (h.channel == ChannelID::mcp) {
+				if (h.detector == DetectorID::pos) {
+					histpositive->Fill(h.time);
 				}
-				else if(h.detector == DetectorID::neg){
-					hist.negative->Fill(hit.time);
-				}
+				//else if(hit.detector == DetectorID::neg){
+				//histnegative->Fill(h.time);
+				//}
 			}
 		}
+		histpositive->Draw();
 	}
+}
 //Draw histograms, which represent time spectra relative to the positron hit
 //Want to export histogram data as well as save time spectra figures
 //need to set up canvas first
-//hist.positive->Draw();
-//hist.negative->Draw();
-}
 
+//hist.negative->Draw();
