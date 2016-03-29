b07de16cc494c58e1b6c0cc8f404063cc8206434
TamaraJBabij <tamarababij@gmail.com>
2016-03-29 16:47:01 +1100

    Setting up cpp

--- app/app.cpp
+++ app/app.cpp
@@ -30,7 +30,15 @@ int main(int argc, char* argv[]) {
 	//tree->Draw("Time");
 	HistogramPair hists = plotTimeSpectraDS(data);
 
-	hists.positive->Draw();	hists.negative->Draw();	rootapp->Run();
+	// need to switch between canvases in root in order to creat two histograms
+	//any instance of TCanvas can be subdivided into independent graphical areas, 
+	//called pads (by default, a canvas contain a single pad, occupying the whole space - TCanvas inherits from TPad).
+
+	TCanvas c1("c1", "First Canvas");
+	//TPad::Divide() specifies number of vertical and horizontal slices of canvas
+	c1.Divide(1,2);
+	c1.cd(1);
+	hists.positive->Draw();	c1.cd(2);	hists.negative->Draw();	rootapp->Run();
 
     return 0;
 }
---
+++ macros/associateHits.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "DataSet.h"
+#include "Group.h"
+#include "Hit.h"
+#include "TTree.h"
+#include "TFile.h"
+
+// 
